a5c03958076f718e739319c0d45fdc426e5119b0
Andranik <tednaaa@gmail.com>
2025-09-08 23:21:26 +0400

    lol

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -3,7 +3,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.padding = { x = 10 y = 10 };
+      window.padding = { x = 10; y = 10; };
 
       font = {
         normal = {
