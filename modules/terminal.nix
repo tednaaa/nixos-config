3a8d608ed543771bf48d0b21b5c3e4b7e124b7b8
Andranik <tednaaa@gmail.com>
2025-09-09 19:47:31 +0400

    update terminal pkgs

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    zellij neovim starship
-    btop ripgrep zoxide eza fd bat fzf jq
-    yazi ueberzugpp resvg tabiew
+    zellij neovim starship fastfetch
+    tabiew btop
+    yazi ueberzugpp resvg
+    fzf jq ripgrep zoxide eza fd bat
     git delta lazygit
     netcat-openbsd openssh rsync bind cmake
     docker docker-compose lazydocker
-    kubectl kubernetes-helm k9s doctl ansible terraform
+    doctl glab kubectl kubernetes-helm k9s ansible opentofu fluxcd
     mise uv pnpm
-    nixd
+
+    # LSP
+    nixd nil gitlab-ci-ls
   ];
 
   virtualisation.docker.enable = true;
