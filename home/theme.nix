f016dd6f78edeb35ef48150ca6924149439bdbf4
Andranik <tednaaa@gmail.com>
2025-12-13 22:39:08 +0400

    fix gtk dark theme

--- home/theme.nix
+++ home/theme.nix
@@ -10,33 +10,27 @@
 
   gtk = {
     enable = true;
-
+    colorScheme = "dark";
     theme = {
       package = pkgs.orchis-theme;
       name = "Orchis-Dark";
     };
-
     iconTheme = {
       package = pkgs.papirus-icon-theme;
       name = "Papirus-Dark";
     };
-
     font = {
       name = "Noto Sans Medium";
       size = 11;
     };
-
-    gtk3.extraConfig = ''
-      gtk-application-prefer-dark-theme=1
-    '';
-    gtk4.extraConfig = ''
-      gtk-application-prefer-dark-theme=1
-    '';
   };
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      gtk-theme = "Orchis-Dark";
+      icon-theme = "Papirus-Dark";
+      cursor-theme = "macOS";
     };
   };
 }
