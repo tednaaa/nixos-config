405a7f7643bef724481bfbe29fda44a2e187d53a
Andranik <tednaaa@gmail.com>
2025-12-24 13:08:58 +0400

    fix network dns

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -1,13 +1,13 @@
 { ... }:
 {
   imports = [
-    ../modules/apps.nix
-    ../modules/base.nix
-    ../modules/fonts.nix
-    ../modules/gaming.nix
-    ../modules/dev.nix
+    ../modules/core.nix
     ../modules/users.nix
     ../modules/niri.nix
+    ../modules/fonts.nix
+    ../modules/dev.nix
+    ../modules/apps.nix
+    ../modules/gaming.nix
 
     ./hardware-configuration.nix
   ];
--- modules/core.nix
+++ modules/core.nix
@@ -25,6 +25,12 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+    "8.8.8.8"
+  ];
 
   time.timeZone = "Asia/Yerevan";
   i18n.defaultLocale = "en_US.UTF-8";
