6cff32ecec84cefe15d9df11680008eeb87e99c6
Andranik <tednaaa@gmail.com>
2025-12-25 23:15:08 +0400

    add ely prism launcher

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -6,4 +6,23 @@
       proton-ge-bin
     ];
   };
+
+  environment.systemPackages = with pkgs; [
+    (prismlauncher.override {
+      prismlauncher-unwrapped = prismlauncher-unwrapped.overrideAttrs (old: {
+        src = fetchFromGitHub {
+          owner = "ElyPrismLauncher";
+          repo = "ElyPrismLauncher";
+          rev = "9.5";
+          hash = "sha256-gGG3A/UezlT3Tq8HMo3mJg7CS/vDByEeh1QxId0Qf2s=";
+        };
+
+        pname = "elyprismlauncher";
+        version = "9.5";
+      });
+
+      jdks = [ jdk21 ];
+      gamemodeSupport = true;
+    })
+  ];
 }
