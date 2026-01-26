e6856e75f9db8858a8909847bca099644c792b40
Andranik <tednaaa@gmail.com>
2026-01-26 16:10:54 +0400

    add services xkb layout config

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,8 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs = {
     niri.enable = true;
   };
+
+  # options picker
+  # bat $(nix-build --no-out-link '<nixpkgs>' -A xkeyboard_config)/share/X11/xkb/rules/base.lst | fzf
+  services.xserver.xkb = {
+    layout = "us,ru";
+    options = "ctrl:grouptoggle_capscontrol";
+  };
+  console.useXkbConfig = lib.mkForce true;
 
   services = {
     displayManager.gdm.enable = true;
