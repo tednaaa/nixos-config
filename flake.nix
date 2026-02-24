24fe5128406ee174d4f5df727c026782285d726a
Andranik <tednaaa@gmail.com>
2026-02-24 22:52:08 +0400

    use nixos 25.11 for base system and unstable for dev tools

--- flake.nix
+++ flake.nix
@@ -2,37 +2,51 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      username = "tedna";
-      hostname = "nixos";
-    in {
-      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs username hostname;
-        };
-        modules = [
-          ./hosts/nixos.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home/main.nix;
-            home-manager.backupFileExtension = "backup";
-            home-manager.extraSpecialArgs = {
-              inherit inputs username;
-            };
-          }
-        ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    username = "tedna";
+    hostname = "nixos";
+
+    pkgs-unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
     };
+  in {
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs username hostname pkgs-unstable;
+      };
+      modules = [
+        ./hosts/nixos.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.${username} = import ./home/main.nix;
+            backupFileExtension = "backup";
+            extraSpecialArgs = {
+              inherit inputs username pkgs-unstable;
+            };
+          };
+        }
+      ];
+    };
+  };
 }
--- modules/dev.nix
+++ modules/dev.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   # run dynamically linked executables, need for zed
   programs.nix-ld = {
     enable = true;
@@ -15,69 +18,67 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    zed-editor
-    neovim
-    cursor-cli
-    claude-code-bin
-    # code-cursor
-    # opencode
+  environment.systemPackages =
+    (with pkgs-unstable; [
+      zed-editor
+      neovim
+      cursor-cli
+      claude-code-bin
 
-    godot
+      godot
 
-    zellij
-    starship
-    fastfetch
-    postgresql_18
-    dbeaver-bin
-    dbgate
-    tabiew
-    yazi
-    ueberzugpp
-    resvg
+      zellij
+      starship
+      fastfetch
+      postgresql_18
+      dbeaver-bin
+      dbgate
+      tabiew
+      yazi
+      ueberzugpp
+      resvg
 
-    fzf
-    skim
-    jq
-    ripgrep
-    zoxide
-    eza
-    fd
-    bat
-    git
-    delta
-    lazygit
-    (callPackage ../packages/resto.nix { })
+      fzf
+      skim
+      jq
+      ripgrep
+      zoxide
+      eza
+      fd
+      bat
+      git
+      delta
+      lazygit
 
-    # TODO: not working, glibc version is not compatible
-    # netcat-openbsd
-    openssh
-    rsync
-    bind
-    cmake
-    zrok
+      devenv
+      uv
+      pnpm
 
-    docker-buildx
-    docker-compose
-    lazydocker
+      nixd
+      nil
+      nginx-language-server
+      gitlab-ci-ls
 
-    doctl
-    glab
-    kubectl
-    kubernetes-helm
-    k9s
-    ansible
-    opentofu
-    fluxcd
+      docker-buildx
+      docker-compose
+      lazydocker
 
-    devenv
-    uv
-    pnpm
+      doctl
+      glab
+      kubectl
+      kubernetes-helm
+      k9s
+      ansible
+      opentofu
+      fluxcd
+    ])
+    ++ (with pkgs; [
+      openssh
+      rsync
+      bind
+      cmake
+      zrok
 
-    # LSP
-    nixd
-    nil
-    nginx-language-server
-    gitlab-ci-ls
-  ];
+      (callPackage ../packages/resto.nix {})
+    ]);
 }
