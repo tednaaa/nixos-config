e70160f5acc1d4ba9e4bd8ddb6716c9c140d2ace
Andranik <tednaaa@gmail.com>
2026-03-09 01:25:11 +0400

    direnv usage deprecated

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,3 +1,5 @@
+# deprecated: only needed for devenv v1, but devenv v2 not need direnv
+# before migrating need fish support: https://github.com/cachix/devenv/issues/2487
 { ... }:
 {
   programs.direnv = {
