5959a479bc99511636709bb5673852e0355f3ff3
Andranik <tednaaa@gmail.com>
2026-08-03 18:05:16 +0400

    fix zed alias in config.fish

--- home/fish/config.fish
+++ home/fish/config.fish
@@ -1,14 +1,14 @@
 fish_add_path ~/.local/bin
 
+alias ls="eza --all --icons -l --no-user -o"
+alias fd="fd --hidden"
+alias k="kubectl"
+alias zed="zeditor"
+
 set -gx EDITOR "zeditor --wait"
 # set -gx EDITOR "nvim"
 
 if status is-interactive
-	alias ls="eza --all --icons -l --no-user -o"
-	alias fd="fd --hidden"
-	alias k="kubectl"
-	alias zed="zeditor"
-
 	devenv hook fish | source
 	starship init fish | source
 	zoxide init fish | source
