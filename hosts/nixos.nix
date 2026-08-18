33ad1b2a1103841b041d44f79952108776411125
Andranik <tednaaa@gmail.com>
2026-08-18 23:31:15 +0400

    cleanup hosts/nixos.nix

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -2,17 +2,17 @@
 {
   imports = [
     ../modules/core.nix
-    ../modules/nvidia.nix
     ../modules/networking.nix
     ../modules/users.nix
-    ../modules/niri.nix
     ../modules/media.nix
     ../modules/fonts.nix
-    ../modules/dev.nix
-    ../modules/pentest.nix
-    ../modules/docker.nix
     ../modules/apps.nix
+    ../modules/dev.nix
+    ../modules/docker.nix
     ../modules/gaming.nix
+    ../modules/nvidia.nix
+    ../modules/niri.nix
+    # ../modules/pentest.nix
 
     ./hardware-configuration.nix
   ];
