523217b06402653bf8cc0c263c81fcfdc11073a3
Andranik <tednaaa@gmail.com>
2026-08-31 12:57:02 +0400

    update relkit

--- packages/relkit.nix
+++ packages/relkit.nix
@@ -1,33 +1,50 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
   makeWrapper,
+  installShellFiles,
+  versionCheckHook,
   git,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "relkit";
-  version = "0.2.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "tednaaa";
     repo = "relkit";
     rev = "v${version}";
-    hash = "sha256-ikQBox2Uq0A3sKBRaxa3P8C/oESQM9mC+Sbu1J6JlJY=";
+    hash = "sha256-9MNhlLA7yJYo+X4+/SG2JduFTgxGVnphevM7lWwh4nk=";
   };
 
-  cargoHash = "sha256-Dc969hGC0mH5wRbV86gO2RkfjgZAcLYy/tjJ3TEOyXg=";
+  cargoHash = "sha256-RGvxWuoPH1zfxeEkWsavTDM+G2EoeUAwFdnTRj7Dt5s=";
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [
+    makeWrapper
+    installShellFiles
+  ];
 
-  postInstall = ''
-    wrapProgram $out/bin/relkit --prefix PATH : ${lib.makeBinPath [ git ]}
-  '';
+  postInstall =
+    lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+      installShellCompletion --cmd relkit \
+        --bash <($out/bin/relkit --completions bash) \
+        --fish <($out/bin/relkit --completions fish) \
+        --zsh <($out/bin/relkit --completions zsh)
+    ''
+    + ''
+      wrapProgram $out/bin/relkit --prefix PATH : ${lib.makeBinPath [ git ]}
+    '';
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
 
   meta = with lib; {
     description = "Interactive release tool that bumps the version, writes a changelog, then tags and pushes";
     homepage = "https://github.com/tednaaa/relkit";
+    changelog = "https://github.com/tednaaa/relkit/blob/v${version}/CHANGELOG.md";
     license = licenses.gpl3Only;
     maintainers = [ ];
     mainProgram = "relkit";
