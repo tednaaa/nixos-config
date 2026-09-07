7f34245873e7ad17a195fd664fc257e02a95f5a2
Andranik <tednaaa@gmail.com>
2026-09-07 15:46:51 +0400

    fish: add find-npm-dep function

--- home/fish/conf.d/find-npm-dep.fish
+++ home/fish/conf.d/find-npm-dep.fish
@@ -1,19 +1,24 @@
-function show_shonk_ui_projects
+function find_npm_dep -a dep
+	if test -z "$dep"
+		echo (set_color red)"usage: find_npm_dep <package>"(set_color normal) >&2
+		return 1
+	end
+
 	set -l filter '
 		(.dependencies + .devDependencies + .peerDependencies)
-		| .["shonk-ui"] // empty
+		| .[$dep] // empty
 		| "\(input_filename)\t\(.)"
 	'
 
-	set -l rows (fd -t f -g package.json ~ -E node_modules -E '.*' -X jq -r $filter | sort)
+	set -l rows (fd -t f -g package.json ~ --no-ignore-vcs -E dist -E '.*' -X jq -r --arg dep $dep $filter | sort)
 
 	if test -z "$rows"
-		echo (set_color yellow)"no projects use shonk-ui"(set_color normal)
+		echo (set_color yellow)"no projects use $dep"(set_color normal)
 		return 1
 	end
 
 	echo
-	echo "  "(set_color -o cyan)shonk-ui(set_color normal)(set_color brblack)" · "(count $rows)" projects"(set_color normal)
+	echo "  "(set_color -o cyan)$dep(set_color normal)(set_color brblack)" · "(count $rows)" projects"(set_color normal)
 	echo
 
 	for row in $rows
